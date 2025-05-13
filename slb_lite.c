9b9f789c0b2d3f9e1bcbabe521c13e93350955a0
Gabriel Kaizer de Menezes <g.menezes@edu.pucrs.br>
2025-05-13 01:51:02 -0300

    Update: improve read logic + adjusted shift bug

--- slb_lite.c
+++ slb_lite.c
@@ -43,7 +43,7 @@ static int slb_master_transfer(const struct device_s *dev, uint8_t data)
 		_delay_us(config->sync_time); // delay de sync de 33us
 		
 		config->gpio_sdl(1); // set data high
-		_delay_us((((data & 0x80) ==  len_data>>7) + 1) *config->sync_time); // delay de sync de 33us
+		_delay_us((((data & 0x80) ==  len_data<<7) + 1) *config->sync_time); // delay de sync de 33us
 
 		data <<= 1; // pega o proximo bit
 	}
@@ -55,26 +55,25 @@ static int slb_read_bit(const struct device_s *dev)
 {
 	uint8_t bit;
   	struct slb_config_s *config;
-	int counter = 0;
+	int counter = 0, val;
+	uint64_t lasttime = 0, actualtime = 0;
 
 	config = (struct slb_config_s *)dev->config;
 
-	while(!config->gpio_sdl(-1)); // espera de fato o bit começar a ser enviado 
+	while(!config->gpio_sdl(-1)); // espera de fato o bit começar a ser enviado (tempo de sync)
 
-	while (1)
-	{
-		if(config->gpio_sdl(-1) == 1) { // se o barramento estiver em low, significa que o slave ta respondendo
-			if(counter > 8) { // maior que 80us é stop bit
-				return -1; // stop bit
-			} else if (counter > 4) { // entre 80us e 40us é bit 1
-				return 1; // bit 1
-			} else { // menor que 40us é bit 0
-				return 0; // bit 0
-			}
-		} else { // entra aqui quando barramento ta em high
-			counter++;
-			_delay_us(10); // verifica de 10 em 10us
-		}
+	lasttime = _read_us(); // pega o tempo atual
+	while (config->gpio_sdl(-1)); // espera o tempo em que o barramento fica em high
+	actualtime = _read_us(); // pega o tempo atual
+
+	val = actualtime - lasttime; // calcula o tempo que ficou em high
+
+	if(val > 80) { // tempo do stop bit
+		return -1; // stop bit
+	} else if (counter > 40) { // entre 80us e 40us é bit 1
+		return 1; // bit 1
+	} else { // menor que 40us é bit 0
+		return 0; // bit 0
 	}
 }
 
@@ -161,7 +160,7 @@ static int slb_driver_open(const struct device_s *dev, int mode)
 		//config->gpio_sdl(0); acho que não seta aqui e sim no write, pensar!
 
 	} else {
-		return -1; // slave mode not supported (does not transmit)
+		return -1; // slave mode não manda open
 	}
 
 	return retval;
@@ -184,7 +183,7 @@ static int slb_driver_close(const struct device_s *dev)
 		_delay_us(100); // delay de sync de 100us para start
 
 	} else {
-		return -1; // slave mode not supported (does not transmit)
+		return -1; // slave mode não manda close
 	}
 
 	CRITICAL_ENTER();
@@ -200,7 +199,7 @@ static size_t slb_driver_read(const struct device_s *dev, void *buf, size_t coun
 	struct slb_data_s *data;
 	uint8_t *p;
 	int i, val = 0;
-	int counter = 0;
+	uint64_t lasttime = 0, actualtime = 0;
 
 	config = (struct slb_config_s *)dev->config;
 	data = (struct slb_data_s *)dev->data;
@@ -208,54 +207,31 @@ static size_t slb_driver_read(const struct device_s *dev, void *buf, size_t coun
 
 	if(!data->init) return -1;
 
-	if(config->device_mode == SLB_MASTER) {
+	NOSCHED_ENTER();
 
-		NOSCHED_ENTER();
+	// espera até que o barramento vá para high
+	while(!config->gpio_sdl(-1)); // espera acabar o tempo de select
+	
+	// espera até que o barramento vá para low
+	lasttime = _read_us(); 
+	while (config->gpio_sdl(-1)); // espera de fato o bit começar a ser enviado
+	actualtime = _read_us(); // pega o tempo atual
 
-		while (1)
-		{
-			if(config->gpio_sdl(-1) == 0) { // se o barramento estiver em low, significa que o slave ta respondendo
-				if(counter > 15) { // maior q 100 e menor que 800, pra abranger um caso onde a leitura começa atrasada (a cada 1 são 10us)
-					break;
-				} else {
-					return -1; // tempo começou errado ou leitura muito atrasada
-				}
-			} else { // entra aqui quando barramento ta em high
-				counter++;
-				_delay_us(10); // verifica de 10 em 10us
-			}
-		}
+	// verifica se o tempo que ficou esperando é o tempo minimo definido para start
+	if(actualtime - lasttime < 80) return -1; // tempo de start muito curto
+
+	// o +1 é porque tem o bit a mais do checksum
+	for(i = 0; i < count + 1; i++) { // número máximo de bytes a serem lidos, se passar desse valor, barramento só não será mais lido
+		val = slb_read_byte(dev); // read data
 		
-		counter = 0;
+		if(val < 0) break; // se for stop bit, sai do loop
 
-		while (1)
-		{
-			if(config->gpio_sdl(-1)) {
-				if(counter > 8) {
-					break;	
-				} else {
-					return -1; // tempo errado pra start
-				}
-			} else {
-				counter++;
-				_delay_us(10); // verifica de 10 em 10us
-			}
-		}
-
-		for(i = 0; i < count; i++) { // número máximo de bytes a serem lidos
-			val = slb_read_byte(dev); // read data
-			
-			if(val < 0) break; // se for stop bit, sai do loop
-
-			p[i] = val; // do contrário, armazena o byte lido
-		}
-
-		NOSCHED_LEAVE();
-
-		return i;
-	} else {
-		return -1; // slave mode not supported (does not receive)
+		p[i] = val; // do contrário, armazena o byte lido
 	}
+
+	NOSCHED_LEAVE();
+
+	return i;
 }
 
 static size_t slb_driver_write(const struct device_s *dev, void *buf, size_t count) 
@@ -273,27 +249,24 @@ static size_t slb_driver_write(const struct device_s *dev, void *buf, size_t cou
 
 	uint32_t checksum = 0;
 
-	if(config->device_mode == SLB_MASTER) {
+	// REMEMBER -> SLAVE JUST TRANSMIT WHEN MASTER SENDS A READ REQUEST
 
-		NOSCHED_ENTER();
+	NOSCHED_ENTER();
 
-		for (i = 0; i < count; i++) { 
-			
-			slb_master_transfer(dev, p[i]);
+	for (i = 0; i < count; i++) { 
 		
-			checksum += p[i]; // soma o checksum
-		}	
+		slb_master_transfer(dev, p[i]);
+	
+		checksum += p[i]; // soma o checksum
+	}	
 
-		slb_master_transfer(dev, (uint8_t)(checksum%256)); // envia o checksum
+	slb_master_transfer(dev, (uint8_t)(checksum%256)); // envia o checksum
 
-		config->gpio_sdl(1); // antes de select sempre ta high
+	config->gpio_sdl(1); // antes de select sempre ta high
 
-		NOSCHED_LEAVE();
+	NOSCHED_LEAVE();
 
-		return i;
-	} else {
-		return -1; // slave mode not supported (does not transmit)
-	}
+	return i;
 }
 
 /* device driver function mapping for generic API */
